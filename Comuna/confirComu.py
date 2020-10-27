e7322b6ed8a12d9186c360563f9b45e8c38214ae
nrivas2017 <31453370+nrivas2017@users.noreply.github.com>
2020-10-27 19:46:50 -0300

    correccion

--- Comuna/confirComu.py
+++ Comuna/confirComu.py
@@ -5,10 +5,11 @@ def archivos(ruta = 'Casos Confirmados/'):
     return listdir(ruta)
 
 nombres =(archivos())
-
+nombres = nombres[:-1]
 
 for j in range(len(nombres)):
-    dDic = {"2020-03-30":{}}
+    dDic = {nombres[j][:-21]:{}}
+    #print (nombres[j])
     data=open('./Casos Confirmados/'+nombres[j])
     reader=csv.reader(data)
 
@@ -20,10 +21,8 @@ for j in range(len(nombres)):
             dd[row[2]]={"Casos Confirmados":row[5]}
         else:
             dd[row[2]]={"Casos Confirmados":row[5]}
-        dDic["2020-03-30"][aux]=dd
-    del dDic["2020-03-30"]["Region"]
+        dDic[nombres[j][:-21]][aux]=dd
+    del dDic[nombres[j][:-21]]["Region"]
     with open('./Casos Confirmados/json/'+nombres[j][:-3]+'json','w') as file:
         json.dump(dDic,file, indent=4)
-
-
-        
+       
