bdfc6d13e25702d0c43f65c7851859e90050d186
nrivas2017 <31453370+nrivas2017@users.noreply.github.com>
2020-10-27 22:07:49 -0300

    Avance de front-end streamlit

--- api.py
+++ api.py
@@ -1,11 +1,73 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+from datetime import datetime
  
- 
-st.title('Título')
-st.header("Cabecero")
-st.subheader("SubCabecero")
-st.text("Esto es texto")
-st.latex("y = x^2")
-st.code("if a == 1:\n    print(a)", language="python")
-st.code("var a = 1;", language="javascript")
-st.markdown("Esto es **texto** usando *Markdown*")
+
+
+
+
+
+
+
+
+
+
+
+
+#################### TITULO
+st.title('Predicción del covid-19 en Chile')
+
+
+############# EJEMPLOS CABECERA
+#st.header("EJEMPLO Cabecero")
+#st.subheader("EJEMPLO SubCabecero")
+
+
+opciones_grafico = ['1', '2', '3']
+
+option = st.selectbox('Elegir gráfico',opciones_grafico)
+st.write('Gráfico seleccionado:', option)
+
+st.subheader(option)
+
+################  GRAFICO #################
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+###########################################
+
+
+################  CHECKBOX GRAFICO
+if st.checkbox('mostrar segundo gráfico'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    st.line_chart(chart_data)
+###########################################
+
+
+################  SIDEBAR SELECTBOX
+
+st.sidebar.header("Navegación")
+
+opcion_nav = st.sidebar.radio("",('Casos', 'Defunciones Registro Civil', 'Datos Deis','Ocupación Hospitalaria','Positivad Diaria'))
+
+
+
+
+options = st.sidebar.multiselect('Elegir regiones',['1', '2', '3', '4'],['1', '3'])
+#st.sidebar.write('You selected:', options)
+
+
+
+###########################################
+
+
+
+
+st.write('Autores: Nicolás Rivas, Marco Pedemonte, Ignacio Ringler, Juan Pérez')
