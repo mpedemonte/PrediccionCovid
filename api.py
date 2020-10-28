da3babaea717f1938fd5f27290c09edf1bddc46a
nrivas2017 <31453370+nrivas2017@users.noreply.github.com>
2020-10-28 14:08:21 -0300

    Comandos para entorno virtual

--- api.py
+++ api.py
@@ -1,28 +1,30 @@
+#CREACION ENTORNO VIRTUAL EN CONDA LLAMADO STREAMLIT
+#       conda create -n streamlit -y
+
+
+#ACTIVAR ENTORNO VIRTUAL
+#          conda activate streamlit
+
+
+#INSTALAR STREAMLIT
+#    pip install streamlit
+
+
+
 import streamlit as st
 import pandas as pd
 import numpy as np
 import time
 from datetime import datetime
- 
-
-
-
-
-
-
-
-
-
-
 
 
 #################### TITULO
 st.title('Predicción del covid-19 en Chile')
 
-
-############# EJEMPLOS CABECERA
+############# EJEMPLOS CABECERA ###############
 #st.header("EJEMPLO Cabecero")
 #st.subheader("EJEMPLO SubCabecero")
+###############################################
 
 
 opciones_grafico = ['1', '2', '3']
@@ -50,24 +52,34 @@ if st.checkbox('mostrar segundo gráfico'):
     st.line_chart(chart_data)
 ###########################################
 
+st.write('Autores: Marco Pedemonte, Juan Pérez, Ignacio Ringler, Nicolás Rivas')
 
-################  SIDEBAR SELECTBOX
+
+
+
+
+
+
+
+
+#########################################################
+#########################################################
+#########################################################
+####################  SIDEBAR  ##########################
+#########################################################
+#########################################################
+#########################################################
 
 st.sidebar.header("Navegación")
 
 opcion_nav = st.sidebar.radio("",('Casos', 'Defunciones Registro Civil', 'Datos Deis','Ocupación Hospitalaria','Positivad Diaria'))
 
-
-
-
 options = st.sidebar.multiselect('Elegir regiones',['1', '2', '3', '4'],['1', '3'])
 #st.sidebar.write('You selected:', options)
 
+#########################################################
+#########################################################
 
 
-###########################################
 
 
-
-
-st.write('Autores: Nicolás Rivas, Marco Pedemonte, Ignacio Ringler, Juan Pérez')
